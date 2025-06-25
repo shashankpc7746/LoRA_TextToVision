5a2d82083f7699520aeeaf0a098597b22d6d6923
shashankpc7746 <shashankpc7746@gmail.com>
2025-06-25 14:18:56 +0530

    local webhosting done

--- AnimateDiff_API/streamlit_app.py
+++ AnimateDiff_API/streamlit_app.py
@@ -352,7 +352,7 @@ if submit:
         time.sleep(0.5)
 
         # Send request to FastAPI backend
-        api_url = "http://127.0.0.1:8000/generate-video"
+        api_url = "https://997b-157-119-202-213.ngrok-free.app/generate-video"
 
         status_text.markdown('<div class="status-text">🧠 AI model loading and initializing...</div>', unsafe_allow_html=True)
         progress_bar.progress(15)
